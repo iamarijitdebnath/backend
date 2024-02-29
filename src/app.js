683e554e6f4faf721a7ecd007fedb15aa38a1b66
Arijit Debnath <arijitdebnath34@gmail.com>
2024-03-01 01:02:37 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -17,11 +17,17 @@ app.use(express.urlencoded({
     extended:true,
     limit:"16kb"
 }))
-
 app.use(express.static("public"))
-
-
 app.use(cookieParser())
+
+
+// router import
+
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
 
 
 
